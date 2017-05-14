330bd278a11e29073a402918b345ce484e5e1a7d
favioSabalero <favio.oviedo.211@gmail.com>
2017-05-14 19:20:05 -0300

    ya estan los queries de usuario

---
+++ scripts/queriesUsuario.js
@@ -0,0 +1,23 @@
+/*
+ fallidos de la ultima semana del usuario
+ */
+var hoy = new Date(), 
+anio = hoy.getYear(), 
+mes = hoy.getMonth(), 
+dia = hoy.getDate() - 15,
+hace7Dias = new Date(anio, mes, dia);
+db.getCollection('Log').find({"fechaHora": {
+    $gte: hace7Dias,
+    $lt: hoy
+},
+"exitoso": { $eq: false },
+"usuario": { $eq: "Fede" }
+}).pretty();
+
+/*
+ fallidos por usuario
+ */
+db.Log.aggregate(
+{ $match: { exitoso: false } },
+{ $group: { _id: "$usuario", total : { $sum : 1 } } },
+{ $sort: { total: -1 } });
